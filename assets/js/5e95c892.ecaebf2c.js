2de0daba133bd67b852b5da7f9ee43b49f95c297
Huseyn Ismayilov <coderloff@gmail.com>
2025-04-03 16:40:11 +0400

    Deploy website - based on e682d8a7a212e9e329a2dfa2f95d89e63259f002

---
+++ assets/js/5e95c892.ecaebf2c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkferx=self.webpackChunkferx||[]).push([[647],{8122:(e,r,s)=>{s.r(r),s.d(r,{default:()=>l});s(6540);var u=s(4164),a=s(1769),c=s(204),n=s(2831),t=s(1410),f=s(4848);function l(e){return(0,f.jsx)(a.e3,{className:(0,u.A)(c.G.wrapper.docsPages),children:(0,f.jsx)(t.A,{children:(0,n.v)(e.route.routes)})})}}}]);
---
+++ assets/js/a7456010.16a14579.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkferx=self.webpackChunkferx||[]).push([[235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
